14e9e993b617117f4c53ec87138c2061cd6e03db
Moacyr <157326098+Mjunior70@users.noreply.github.com>
2026-02-15 21:55:38 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,12 +3,23 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Webhook WhatsApp está online 🚀');
+const VERIFY_TOKEN = "meu_token_secreto";
+
+app.get('/webhook', (req, res) => {
+  const mode = req.query['hub.mode'];
+  const token = req.query['hub.verify_token'];
+  const challenge = req.query['hub.challenge'];
+
+  if (mode && token === VERIFY_TOKEN) {
+    console.log("Webhook verificado!");
+    res.status(200).send(challenge);
+  } else {
+    res.sendStatus(403);
+  }
 });
 
 app.post('/webhook', (req, res) => {
-  console.log(req.body);
+  console.log(JSON.stringify(req.body, null, 2));
   res.sendStatus(200);
 });
 
